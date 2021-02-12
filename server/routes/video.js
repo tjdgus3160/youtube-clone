8872fa9809bab1243d4d35185fa1be28d9aceb32
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-12 11:12:54 +0900

    비디오 저장

--- server/routes/video.js
+++ server/routes/video.js
@@ -3,11 +3,39 @@ const router = express.Router();
 const { Video } = require("../models/Video");
 
 const { auth } = require("../middleware/auth");
+const multer = require('multer');
 
 //=================================
 //             Video
 //=================================
+let storage = multer.diskStorage({
+    destination: (req,file, cb) => {    // 파일 저장 위치
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`); // 파일 저장 이름
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        if (ext !== '.mp4'){
+            return cb(res.status(400).end('only mp4 is allowed'), false);
+        }
+        cb(null, true);
+    }
+});
 
-router.post('/api/video')
+const upload = multer({storage}).single('file');
+
+router.post('/uploadfiles', (req, res) => {
+    // 비디오를 서버에 저장한다.
+    upload(req, res, err => {
+        if(err){
+            return res.json({success: false, err});
+        }
+        return res.json({success: true, url: res.req.file.path, fileName: res.req.file.filename});
+    })
+})
+
+
 
 module.exports = router;
