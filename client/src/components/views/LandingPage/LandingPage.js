609b56024f2887fb07f1e77e2c939358a0ee0278
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-16 16:19:30 +0900

    side video

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { FaCode } from "react-icons/fa";
 import { Card, Icon, Avatar, Col, Typography, Row } from 'antd';
 import Axios from 'axios';
 import moment from 'moment';
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Row, Col, List, Avatar } from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 
 function VideoDetailPage(props) {
 
@@ -42,7 +43,7 @@ function VideoDetailPage(props) {
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
-                    Side Videos
+                    <SideVideo />
                 </Col>
             </Row>
         )
